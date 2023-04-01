0770c87518700f03e70fb6710f8003da7d0de194
kamimore <negivipin084@gmail.com>
2023-04-02 01:50:54 +0530

    using turbolink for spa

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,19 +5,35 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Livewire</title>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    {{-- @livewireStyles --}}
+    <livewire:styles />
 </head>
 
 <body>
-    <nav>
-        <li>
-            <a href="/home">Home</a>
-        </li>
-        <li>
-            <a href="/login">Login</a>
-        </li>
-    </nav>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">Welcome</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item active">
+              <a class="nav-link" href="/home">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="/login">Login</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
     @yield('content')
+
+    <livewire:scripts />
 </body>
 
 </html>
--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -1,5 +1,10 @@
 @section('content')
-    <div>
-        I am home
+    <div class="d-flex align-items-stretch justify-content-around mx-2 mx-lg-5 mt-4 mt-lg-4 flex-column flex-lg-row">
+        <div class="col col-lg-4 card card-body mx-0 mx-lg-4 mb-4 mb-lg-0">
+            <livewire:tickets />
+        </div>
+        <div class="col col-lg-8 card card-body">
+            <livewire:comments />
+        </div>
     </div>
 @endsection
