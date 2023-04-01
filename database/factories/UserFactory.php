854b5c3515fa1371f38390fefa6db80c45365e32
kamimore <negivipin084@gmail.com>
2023-04-01 10:18:05 +0530

    defining factories for user

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
 
 class UserFactory extends Factory
@@ -18,9 +19,19 @@ class UserFactory extends Factory
             'name' => $this->faker->name(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => Hash::make('password'),
             'remember_token' => Str::random(10),
         ];
+    }
+
+    public function adminUser(){
+        return $this->state([
+            'name'=> 'vipin singh negi',
+            'email'=> 'negivipin996@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('vipin'),
+            'remember_token' => Str::random(10),
+        ]);
     }
 
     /**
